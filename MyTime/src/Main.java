d8a103b2140dbc001ef03c9e77e52495c99f7a2b
v02qabs <takesue090@gmail.com>
2018-09-27 21:22:22 +0900

    date

--- MyTime/src/Main.java
+++ MyTime/src/Main.java
@@ -4,5 +4,9 @@ public class Main
 {
 	public static void main(String[] args)
 	{
+	    Date d = new Date();
+	    System.out.println("date = " + d);
+			System.setProperty("usr.dir", "/storage/sdcard0");
+			System.out.println("get = " + System.getProperty("usr.dir"));
 	}
 }
